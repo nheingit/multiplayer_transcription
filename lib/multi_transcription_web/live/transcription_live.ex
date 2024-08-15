0c80e4c1424361b0fec83efe4229e57706f0f4bc
nheingit <60185486+nheingit@users.noreply.github.com>
2024-08-15 12:48:09 -0500

    update liveview

--- lib/multi_transcription_web/live/transcription_live.ex
+++ lib/multi_transcription_web/live/transcription_live.ex
@@ -11,17 +11,6 @@ defmodule MultiTranscriptionWeb.TranscriptionLive do
     {:noreply, assign(socket, transcriptions: transcriptions)}
   end
 
-  def handle_event("upload_audio", %{"audio" => %Plug.Upload{path: path}}, socket) do
-    MultiTranscription.Audio.speech_to_text(path, 20, fn ss, text ->
-      MultiTranscriptionWeb.Endpoint.broadcast("transcription:lobby", "new_transcription", %{
-        "timestamp" => ss,
-        "transcription" => text
-      })
-    end)
-
-    {:noreply, socket}
-  end
-
   def handle_info(
         %Phoenix.Socket.Broadcast{
           topic: "transcription:lobby",
@@ -43,12 +32,10 @@ defmodule MultiTranscriptionWeb.TranscriptionLive do
           <li><strong><%= timestamp %>:</strong> <%= transcription %></li>
         <% end %>
       </ul>
-      <form phx-submit="upload_audio">
+      <form>
         <input type="file" id="audio-upload" name="audio" accept=".mp3" phx-hook="AudioUpload" />
-        <button type="submit">Upload MP3</button>
       </form>
     </div>
     """
   end
 end
-
